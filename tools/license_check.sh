b37dcecb52ffdeeaf7eb1e0b2c7bdcf1f533ac7e
Björn Döbel <doebel@os.inf.tu-dresden.de>
2012-07-25 18:38:50 +0200

    add license

--- tools/license_check.sh
+++ tools/license_check.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-for f in $(find . -name *.h) $(find . -name *.cpp); do echo -n $f; head -2 $f | tail -1; done
+for f in $(find . -name *.h) $(find . -name *.cpp); do
+		echo -n $f; head -2 $f | tail -1 | grep -v "\^\./build";
+done
