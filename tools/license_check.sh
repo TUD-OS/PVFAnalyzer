b46bd55cef6d7b6c24dccaa0716ca67b89156501
Bjoern Doebel <doebel@tudos.org>
2012-08-12 23:22:06 +0200

    license update

--- tools/license_check.sh
+++ tools/license_check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DIRS="analyzer common testing"
+DIRS="cfg_reader cfg_printer common testing"
 for d in $DIRS; do
 	for f in $(find $d -name *.h) $(find $d -name *.cpp); do
 			echo -n $f; head -2 $f | tail -1;
