f1ad6492f417a8f57cbf87c88e008052012a4128
Björn Döbel <doebel@os.inf.tu-dresden.de>
2012-08-16 16:42:05 +0200

    issue update

--- common/instructionProps_udis86.cpp
+++
@@ -1,7 +0,0 @@
-#include "instruction/instruction_udis86.h"
-
-InstructionProperty::OperandAccessInfo
-InstructionPropertyUdis86::propertyForOperand(Instruction* i, unsigned num)
-{
-	return OAI_NONE;
-}
