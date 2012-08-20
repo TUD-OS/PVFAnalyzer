b70398ccc79f85dbdacd184cfc4bbc0cad88ecd8
Björn Döbel <doebel@os.inf.tu-dresden.de>
2012-08-20 15:03:25 +0200

    more call testcases

---
+++ testing/scripts/009-callchain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source $BASEDIR/testing/scripts/global.sh.inc
+if [ $? != 0 ]; then
+	exit 1;
+fi
+
+$ROOTDIR/build/reader/reader -f $TESTCASES/callchain.bin -o $TESTOUT/callchain.cfg -e 0x2d
+$ROOTDIR/build/printer/printer -f $TESTOUT/callchain.cfg -c -o -
---
+++ testing/scripts/010-callhierarchy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source $BASEDIR/testing/scripts/global.sh.inc
+if [ $? != 0 ]; then
+	exit 1;
+fi
+
+$ROOTDIR/build/reader/reader -f $TESTCASES/callhierarchy.bin -o $TESTOUT/callhierarchy.cfg -e 0x16
+$ROOTDIR/build/printer/printer -f $TESTOUT/callhierarchy.cfg -c -o -
